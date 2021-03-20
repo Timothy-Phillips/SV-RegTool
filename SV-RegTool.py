af4391e1a054b39f335af8ce86638a65febf7fe0
Timothy Phillips <timothyphillips1999@gmail.com>
2021-03-20 14:52:29 -0400

    Initializes Python file

---
+++ SV-RegTool.py
@@ -0,0 +1,4 @@
+# SV-RegTool
+# This simple program allows users to select a single variable dataset (.csv)
+# and have it and an adjustable regression model displayed. It will also allow
+# users to query the model for predictions once the model is prepared.
